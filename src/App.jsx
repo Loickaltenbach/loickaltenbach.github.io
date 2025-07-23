f8cffa55c536d601331798bea4426da9111e9b79
Loic Kaltenbach <Loickaltenbach@icloud.com>
2025-07-23 11:53:36 +0200

    fix: Remove mouse activity tracking to simplify App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
@@ -7,19 +6,6 @@ import Footer from './components/Footer'
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    // Tracker l'activité de la souris pour la protection anti-bot
-    const handleMouseMove = () => {
-      sessionStorage.setItem('mouseActivity', 'true')
-    }
-
-    document.addEventListener('mousemove', handleMouseMove)
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove)
-    }
-  }, [])
-
   return (
     <div className="App">
       <Header />
