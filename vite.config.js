8d512e9b1a59ecf61f21050ce352dce8f3e8b896
Loic Kaltenbach <Loickaltenbach@icloud.com>
2025-07-24 19:23:13 +0200

    refactor: Remove ssr plugin from Vite configuration to simplify setup

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
-import ssr from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
   plugins: [
     react(),
-    ssr({ prerender: true }),
     visualizer({
       open: true,
       filename: 'stats.html',
